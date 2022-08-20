f4b30aef8af1cf0cfa8817923375004e8565a2f7
Pavol <palolantaj@gmail.com>
2022-08-20 12:29:16 +0200

    todo working

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import TodoList from './TodoList';
 
 function App() {
 
-  const [todos, setTodos] =  useState([])
+  const [todos, setTodos] =  useState(['Todo 1','Todo 2'])
 
 
   return (
     <>
-    <TodoList />
+    <TodoList todos={todos}/>
     <input type="text"/>
     <button>Add Todo</button>
     <button>Clear Completed Todos</button>
